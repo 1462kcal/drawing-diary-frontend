5932e8a64e78c6331827e274f514501860d7541f
yeon <108470143+1462kcal@users.noreply.github.com>
2026-08-26 18:41:39 +0900

    refactor: Home 사이드바에 네비게이션과 작성 CTA 추가

--- src/features/home/components/Sidebar.tsx
+++ src/features/home/components/Sidebar.tsx
@@ -1,4 +1,5 @@
 import ProfileSummary from "./ProfileSummary";
+import HomeNavigation from "./HomeNavigation";
 import type { User } from "../types/user";
 import LogoutButton from "../../../components/common/LogoutButton";
 
@@ -11,17 +12,45 @@ export default function Sidebar({ user, loading = false }: SidebarProps) {
   return (
     <aside
       style={{
-        width: 220,
+        width: 230,
         flexShrink: 0,
+        minHeight: "calc(100vh - 64px)",
+        padding: "28px 18px",
+        boxSizing: "border-box",
+        borderRight: "1px solid #E8E3DB",
+        background: "#F8F5F0",
+        display: "flex",
+        flexDirection: "column",
       }}
     >
       <ProfileSummary user={user} loading={loading} />
 
-      {/* 로그인 상태에서만 로그아웃 버튼 표시 */}
+      <div style={{ marginTop: 28 }}>
+        <HomeNavigation />
+      </div>
+
+      <div style={{ marginTop: 28 }}>
+        <button
+          style={{
+            width: "100%",
+            padding: "12px 14px",
+            border: 0,
+            borderRadius: 12,
+            background: "#34312D",
+            color: "#fff",
+            fontWeight: 700,
+            cursor: "pointer",
+          }}
+        >
+          ✦ 그림일기 쓰기
+        </button>
+      </div>
+
       {!loading && user && (
         <div
           style={{
-            marginTop: 12,
+            marginTop: "auto",
+            paddingTop: 24,
           }}
         >
           <LogoutButton />
